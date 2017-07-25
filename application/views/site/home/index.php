f0ce19e58e9c4cc8fef24da4af9337924d149a28
cuongfpt <nguyencuong.fpt2013@gmail.com>
2017-07-25 10:00:20 +0700

    update

--- application/views/site/home/index.php
+++ application/views/site/home/index.php
@@ -11,8 +11,13 @@
                         <?php foreach ($listhome as $row): ?>
                             <li>
                                 <div class="item_game col-sm-12 col-xs-12 col-md-12 ">
-                                    <a href="<?php echo base_url('bai-viet/' . $row->seolink . '-' . $row->id) ?>"><img
+                                    <?php if($row->description=="" || $row->body==""):?>
+                                    <a href="#"><img
                                             src="<?php echo public_url('uploads/news/' . $row->images) ?>"></a>
+                                    <?php else:?>
+                                         <a href="<?php echo base_url('bai-viet/' . $row->seolink . '-' . $row->id) ?>"><img
+                                            src="<?php echo public_url('uploads/news/' . $row->images) ?>"></a>
+                                    <?php endif?>        
                                 </div>
                             </li>
                         <?php endforeach ?>
